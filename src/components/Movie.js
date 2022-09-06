4159c09c6d07cf393f0292c8bf08718653bec459
Sadap <sada93@citromail.hu>
2022-09-06 11:50:01 +0000

    Update Movie.js and Movie.css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,14 @@
-import '../styles/Movie.css'
+import "../styles/Movie.css";
 
 const Movie = ({ movie }) => {
   return (
-    <div className='card'>
-      <img src={movie.picture} className='poster'></img>
-      <div className='movie-data'>
-        <div className='movie-title'>{movie.title}</div>
-        <div className='movie-relaseDate'>{movie.relaseDate}</div>
-        <div className='movie-genre'>{movie.genre}</div>
-        <div className='movie-imdbScore'>{movie.imdbScore}</div>
+    <div className="card">
+      <img src={movie.picture} className="poster" alt=""></img>
+      <div className="movie-data">
+        <div className="movie-title">{movie.title}</div>
+        <div className="movie-relaseDate">{movie.relaseDate}</div>
+        <div className="movie-genre">{movie.genre}</div>
+        <div className="movie-imdbScore">IMDb Score: {movie.imdbScore}</div>
       </div>
     </div>
   );
