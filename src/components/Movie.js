ac0d8aabac5832c81ff917fd415e3d5c855295c5
Sadap <sada93@citromail.hu>
2022-09-06 13:22:56 +0000

    Update Movie.js and Movie.css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ const Movie = ({ movie }) => {
       <img src={movie.picture} className="poster" alt=""></img>
       <div className="movie-data">
         <div className="movie-title">{movie.title}</div>
-        <div className="movie-relaseDate">{movie.relaseDate}</div>
+        <div className="movie-relaseDate">({movie.relaseDate})</div>
         <div className="movie-genre">{movie.genre}</div>
         <div className="movie-imdbScore">IMDb Score: {movie.imdbScore}</div>
       </div>
