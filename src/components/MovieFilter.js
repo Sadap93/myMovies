f36fb5e900da5c4514b47e25adff179da7a503c3
Sadap <sada93@citromail.hu>
2022-09-20 09:32:55 +0000

    Update 3 files

--- src/components/MovieFilter.js
+++ src/components/MovieFilter.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-const MovieFilter = () => {
-
-const onChangeFilterHandler = (event) => {
-  console.log(event.target.value);
-  
-}
+const MovieFilter = ({ onChangeFilter, selectedGenre }) => {
+  const onChangeFilterHandler = (event) => {
+    onChangeFilter(event.target.value);
+  };
 
   return (
     <>
       <h2 className="main-title">My Favourite Movies</h2>
       <div className="movie-filter">
         <label>Filter by genre:</label>
-        <select  onChange={onChangeFilterHandler}>
+        <select value={selectedGenre} onChange={onChangeFilterHandler}>
           <option value="all">All</option>
           <option value="horror">horror</option>
           <option value="sci-fi">sci-fi</option>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,29 @@
+import React, { useState } from "react";
 import Movie from "./Movie";
+import MovieFilter from "./MovieFilter";
 import { MoviesArray } from "../MoviesArray";
 
 const MovieList = () => {
-  
-  return MoviesArray.map((movie, index) => (
-    <Movie movie={movie} key={`movie-${index}`} />
-  ));
+  const [filteredGenre, setFilteredGenre] = useState();
+
+  const filterChangeHandler = (selectedGenre) => {
+    setFilteredGenre(selectedGenre);
+  };
+
+  const filteredMovie = MoviesArray.filter((movie) => {
+    return movie.genre === filteredGenre;
+  });
+  return (
+    <>
+      <MovieFilter
+        onChangeFilter={filterChangeHandler}
+        selectedGenre={filteredGenre}
+      />
+      {filteredMovie.map((movie, index) => (
+        <Movie movie={movie} key={`movie-${index}`} />
+      ))}
+    </>
+  );
 };
 
 export default MovieList;
